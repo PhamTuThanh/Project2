4a0c32a27243229c992a6d97c27540f35ba3e2c2
thanhthanh <phamtuthanh0410@gmail.com>
2025-03-13 22:25:52 +0700

    minus update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#000211',
+        'secondary': '#5f6fff',
+      }
+    },
   },
   plugins: [],
 }
